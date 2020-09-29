21aa068394ecd2a3729c033d9d4dcb6c0fc085ec
Benjamin Payoux <benjamin.payoux@protonmail.ch>
2020-09-29 09:55:46 +0200

    Add tailwind

--- pages/games/[hash].js
+++ pages/games/[hash].js
@@ -1,19 +1,27 @@
-import styles from "../../styles/Game.module.css";
-
 function Game({ data }) {
+
   return (
-    <div>
-      Game page {data.game_hash}
-      <div className={styles.board}>
-        <div className={styles.fleet}>
+    <div className="w-screen p-5 flex flex-col">
+      <h1 className="text-2xl">Game page {data.game_hash}</h1>
+
+      <div className="py-2"></div>
+
+      <div className="relative pb-full">
+
+        <div className="absolute grid grid-cols-10 grid-rows-10 w-full h-full gap-px max-w-lg max-h-lg">
+          {createSquares().map((square, i) => (
+            <div key={i} className="w-full h-full bg-blue-500"></div>
+          ))}
+        </div>
+
+        <div className="absolute grid grid-cols-10 grid-rows-10 w-full h-full gap-px max-w-lg max-h-lg">
           {data.ships.map((ship, i) => (
             <div
               key={ship.id}
-              className={styles.square}
+              className="p-1"
               style={formatGridAttributes(ship.grid_attributes)}
             >
-              <div className={styles.ship}>
-              </div>
+              <div className="w-full h-full bg-green-500 rounded-full"></div>
             </div>
           ))}
         </div>
@@ -25,9 +33,16 @@ function Game({ data }) {
 export async function getServerSideProps(context) {
   const res = await fetch(`${process.env.API_HOST}/games/${context.params.hash}`);
   const data = await res.json();
-
   return { props: { data } };
 }
+
+const createSquares = () => {
+  const squares = [];
+  for (let i = 0; i < 100; i++) {
+    squares.push(i);
+  }
+  return squares;
+};
 
 const formatGridAttributes = (str) => {
   const res = str
--- pages/index.js
+++ pages/index.js
@@ -1,29 +1,33 @@
-import Head from 'next/head'
-import styles from '../styles/Home.module.css'
-import { useRouter } from 'next/router'
+import Head from "next/head";
+import { useRouter } from "next/router";
 
 export default function Home() {
-  const router = useRouter()
+  const router = useRouter();
 
   async function createGame() {
     const res = await fetch(`${process.env.API_HOST}/games`, {method: 'POST'})
     const data = await res.json()
 
-    router.push(`/games/${data.game_hash}`)
+    router.push(`/games/${data.game_hash}`);
   }
 
   return (
-    <div className={styles.container}>
+    <div>
       <Head>
         <title>Battleship</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main className={styles.main}>
-        <button className="{styles.button}" onClick={createGame}>
-          Create new game
-        </button>
-      </main>
+      <div className="flex flex-col py-20">
+        <h1 className="text-4xl text-red-500 text-center">
+          Welcome to the Battleship!
+        </h1>
+        <div className="self-center">
+          <button className="mt-20 btn-blue" onClick={createGame}>
+            Create new game
+          </button>
+        </div>
+      </div>
     </div>
-  )
+  );
 }
