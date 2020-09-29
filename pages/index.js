7e7047effe40a168846c9a69ec2829fdaf40eda9
Benjamin Payoux <benjamin.payoux@protonmail.ch>
2020-09-29 16:33:53 +0200

    Add open game list

--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,18 @@
 import Head from 'next/head';
 import { useRouter } from 'next/router';
+import * as api from '../api';
 
-export default function Home() {
+function Home({ data }) {
     const router = useRouter();
 
     async function createGame() {
-        const res = await fetch(`${process.env.NEXT_PUBLIC_API_HOST}/games`, {
-            method: 'POST',
-        });
-        const data = await res.json();
+        const data = await api.createGame();
+        router.push(`/games/${data.game_hash}`);
+    }
 
+    async function joinGame(gameHash) {
+        const data = await api.joinGame(gameHash);
+        console.log(data);
         router.push(`/games/${data.game_hash}`);
     }
 
@@ -20,7 +23,7 @@ export default function Home() {
                 <link rel="icon" href="/favicon.ico" />
             </Head>
 
-            <div className="flex flex-col py-20">
+            <div className="flex flex-col pt-20 h-screen">
                 <h1 className="text-4xl text-red-500 text-center">
                     Welcome to the Battleship!
                 </h1>
@@ -29,7 +32,34 @@ export default function Home() {
                         Create new game
                     </button>
                 </div>
+
+                <div className="mx-4 mt-8">
+                    <h2 className="text-xl font-bold px-3 mb-4">Open Games</h2>
+                    <ul className="flex flex-col border-t border-gray-300">
+                        {data.games.map((game, index) => (
+                            <li
+                                key={game.id}
+                                className="border-b border-gray-300 py-3 px-3 flex items-center justify-between"
+                            >
+                                <span>{game.hash}</span>
+                                <button
+                                    className="btn-blue"
+                                    onClick={() => joinGame(game.hash)}
+                                >
+                                    JOIN
+                                </button>
+                            </li>
+                        ))}
+                    </ul>
+                </div>
             </div>
         </div>
     );
 }
+
+export async function getServerSideProps() {
+    const data = await api.getGames();
+    return { props: { data } };
+}
+
+export default Home;
